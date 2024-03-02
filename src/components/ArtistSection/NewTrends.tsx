c7563cbed033af222521f065106ca6700e7912f8
viicruz <VicttorrHugo100@gmail.com>
2024-03-02 18:41:23 -0300

    feat - render trending artists

--- src/components/ArtistSection/NewTrends.tsx
+++ src/components/ArtistSection/NewTrends.tsx
@@ -4,15 +4,15 @@ import { useState, useEffect } from "react";
 import type { Artist } from "@spotify/web-api-ts-sdk";
 
 async function getArtists() {
-    const response = await fetch('/api/artists');
-    const artists = (await response.json()).artists.items;
+    const response = await fetch('/api/get-artists-from-playlist/37i9dQZEVXbMDoHDwVN2tF');
+    const artists = (await response.json()).artists;
     return artists as Artist[];
 }
 
 export function NewTrends() {
     const [artists, setArtist] = useState<Artist[]>([]);
     useEffect(() => {
-        getArtists().then((data) => setArtist(data) );
+        getArtists().then((data) => setArtist(data));
     }, [])
     return (
         <ArtistSection title="Últimas tendências" artists={artists} />
