6a91988091284b69535f0a2f612ed796f0f94f18
viicruz <VicttorrHugo100@gmail.com>
2024-03-02 21:17:12 -0300

    feat - list new trends

--- src/components/ArtistSection/NewTrends.tsx
+++ src/components/ArtistSection/NewTrends.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import type { Artist } from "@spotify/web-api-ts-sdk";
 
 async function getArtists() {
-    const response = await fetch('/api/get-artists-from-playlist/37i9dQZEVXbMDoHDwVN2tF');
+    const response = await fetch('/api/get-artists-from-playlist/37i9dQZF1DX0FOF1IUWK1W');
     const artists = (await response.json()).artists;
     return artists as Artist[];
 }
@@ -12,7 +12,7 @@ async function getArtists() {
 export function NewTrends() {
     const [artists, setArtist] = useState<Artist[]>([]);
     useEffect(() => {
-        getArtists().then((data) => setArtist(data));
+        getArtists().then((data) => setArtist(data) );
     }, [])
     return (
         <ArtistSection title="Últimas tendências" artists={artists} />
